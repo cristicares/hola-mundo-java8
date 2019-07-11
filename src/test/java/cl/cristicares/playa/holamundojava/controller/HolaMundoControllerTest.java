5c1a5754840348133fc4e158f87f1e6424cd7100
Cristina Cares Unquén <cristina.cares@thoughtworks.com>
2019-07-11 17:06:08 -0400

    Test

---
+++ src/test/java/cl/cristicares/playa/holamundojava/controller/HolaMundoControllerTest.java
@@ -0,0 +1,13 @@
+package cl.cristicares.playa.holamundojava.controller;
+
+import org.junit.Test;
+
+public class HolaMundoControllerTest {
+
+    private HolaMundoController controller = new HolaMundoController();
+
+    @Test
+    public void getHolaMundoTest(){
+        controller.getHolaMundo();
+    }
+}
